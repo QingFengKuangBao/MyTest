5c6f93a6ed8948300886350180dfc5a6c0680818
QingFengKuangBao <jiachao4416@gmail.com>
2018-08-23 13:00:11 +0800

    Java

--- Java/ThreadTest.java
+++ Java/ThreadTest.java
@@ -1,3 +1,5 @@
+package cn.basic;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
@@ -13,35 +15,18 @@ public class ThreadTest {
 	public static void main(String[] args) {
 
 		// 继承Thread类 重写 run 方法
-		Thread t = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-
-			}
-		});
+		Thread t = new ExtendThread();
 		t.start();
 
 		// 实现 Runnable接口
-		Runnable r = new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-
-			}
-		};
+		Runnable r = new ImplRunnable();
 		// 构建线程
 		Thread tr = new Thread(r);
 		tr.start();
 
 		// 实现 Callable接口 可以有返回值 FutureTask
-		Callable<Integer> c = new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				// TODO Auto-generated method stub
-				return 10;
-			}
-		};
+		Callable<Integer> c = new ImplCallable();
+
 		FutureTask<Integer> fu = new FutureTask<>(c);
 		// 构建线程
 		Thread tf = new Thread(fu);
@@ -56,36 +41,41 @@ public class ThreadTest {
 
 	}
 
-	// 继承Thread类 重写 run 方法
-	class ExtendThread extends Thread {
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			System.out.println("继承 Thread类           实现多线程.......");
-		}
+}
+
+// 继承Thread类 重写 run 方法
+class ExtendThread extends Thread {
+
+	public ExtendThread() {
+		// TODO Auto-generated constructor stub
 	}
 
-	// 实现 Runnable接口
-	class ImplRunnable implements Runnable {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("继承 Thread类           实现多线程.......");
+	}
+}
 
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			System.out.println("实现 Runnable接口     实现多线程.......");
-		}
+// 实现 Runnable接口
+class ImplRunnable implements Runnable {
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("实现 Runnable接口     实现多线程.......");
 	}
 
-	// 实现 Callable接口 可以有返回值 , 构造 FutureTask对象 new FutureTask<>(Callable)来执行;
-	class ImplCallable implements Callable<Integer> {
+}
 
-		@Override
-		public Integer call() throws Exception {
-			// TODO Auto-generated method stub
-			System.out.println("实现 Callable接口     实现多线程.......");
-			return 10;
-		}
+// 实现 Callable接口 可以有返回值 , 构造 FutureTask对象 new FutureTask<>(Callable)来执行;
+class ImplCallable implements Callable<Integer> {
 
+	@Override
+	public Integer call() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("实现 Callable接口     实现多线程.......");
+		return 10;
 	}
 
 }
